0a11fc239c2a6b3f7bda72e57662b7793901b1ef
RosieChen06 <katekyo.069@gmail.com>
2025-02-15 04:02:22 +0800

    Update adminRoute.js

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,8 +5,8 @@ import upload from '../middleweares/multer.js'
 const adminRouter = express.Router()
 
 adminRouter.post('/add-data', upload.single('image'), addRecord)
-adminRouter.post('/all-rider',readDB)
-adminRouter.post('/week-data',readWeekDB)
+adminRouter.get('/all-rider',readDB)
+adminRouter.get('/week-data',readWeekDB)
 adminRouter.post('/update-data', upload.single('image'), updateDB)
 adminRouter.post('/update-weekdata', upload.single('image'), updateWeekDB)
 adminRouter.post('/missing-parcel', upload.single('image'), missingParcelRegistration)
