3e5b766bd10b71c8675168496720d88ce549f9a1
RosieChen06 <katekyo.069@gmail.com>
2025-02-16 02:15:00 +0800

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -193,13 +193,11 @@ const readDB = async(req, res) => {
         const currentMonthYear = getMonthYear(new Date());
         const prevMonthYear = getMonthYear(prevMonth);
         const explainResult = await riderModel.find({$or: [
-        { date: { $regex: "^2025/1" } },
         { date: { $regex: "^2025/2" } }
     ]}).explain("executionStats");
         console.log("🔍 查詢分析結果:");
         console.dir(explainResult, { depth: null });
         const riders = await riderModel.find({$or: [
-        { date: { $regex: "^2025/1"} },
         { date: { $regex: "^2025/2" } }
     ]});
         res.json({success:true, riders})
